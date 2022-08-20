d511204132d87ab7dcd489d8213bffd97dcdc3dc
Patilo29 <rmanoelvenancio@gmail.com>
2022-08-19 21:47:42 -0300

    aula19/08/022

--- index.php
+++ index.php
@@ -1,4 +1,60 @@
+
 <?php
 //Comentarios em PHP-Linha
 
+//echo "Meu nome é Rosana";
+$idade = 41;
+$nome ="Rosana";
+$Altura = 1.77;
+$casada = true;
+//echo 10 < 12;
+CONST IDADE =5;
+echo IDADE;
+/*if(10>12){
+echo "Mentira";
+}else{
+    echo "10 não é maior que 12";
+}
+$nota1 = 6;
+if($nota1 >= 6){
+    echo "Aprovado";
+}else if($nota1 < 6 && $nota1 >= 2){
+    echo "Ainda tem chance";
+    //Faça um if que calcule a media e escreva na tela
+    //se foi aprovado ou não
+}*/
+/*
+$n1=7;
+$n2=8;
+$media = $n1+$n2;
+$media = $media/2;
+if($media >=6){
+    echo"Aprovado";
+}else{
+    echo"Reprovado";
+}*/
+//concatenação
+$nome =  " teste ";
+echo"<br> Rosana ".$nome." Rosana "."</br>";
+
+/*loop em php
+$contadora = 0;
+do{
+
+    $contadora++;
+
+}while($contadora <10);
+
+$cont =0;
+while($cont < 10){
+    $cont++;
+
+}
+for($i = 0;$i < 10;$i++){
+if($i % 2 == 0){
+    echo "<p>".$i."É Par</p>";
+}else{
+    echo"<p>".$i."É impar</p>";
+}
+}*/
 ?>
